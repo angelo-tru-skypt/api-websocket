46f05ec620664e51b12c11928b90c836c1b6d2e1
angelo-dev <angelomancebo8@gmail.com>
2025-12-23 23:20:49 +0000

    my API deployment

---
+++ app.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.websockets import WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"Mensaje recibido: {data}")
+    except WebSocketDisconnect:
+        print("El cliente esta desconectado")
